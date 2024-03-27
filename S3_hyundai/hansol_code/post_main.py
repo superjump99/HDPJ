3e3525543ec4b0df6812a914eb8ee4ec3c03dcc4
Hansol Kim <khs_9902@naver.com>
2024-03-27 16:32:32 +0900

    Clean up file paths

--- S3_hyundai/hansol_code/post_main.py
+++ S3_hyundai/hansol_code/post_main.py
@@ -6,55 +6,69 @@ import os
 
 
 if __name__ == '__main__':
-    path = "C:/Users/pc/SS-233/hyundai_code"
-    version = "4.1.3"
-    print("💛💛💛 후처리 코드 실행 💛💛💛")
-    for folder_name in os.listdir(f"{path}/S3_hyundai/2.working_done"):
-        file_name = os.listdir(f"{path}/S3_hyundai/0.given_data/{folder_name}/LDR_RAW_PCD/")[0][12:]
+    high_path = 'C:/Users/pc/SS-233/hyundai_code'
+    step1_path = 'S3_hyundai/1.div&remove'
+    step2_path = 'S3_hyundai/2.parsing_done'
+    step3_path = 'S3_hyundai/3.working_done'
+    step4_path = 'S3_hyundai/4.out'
+    space = "00_sample"
+    sequence_set = "HKMC-N2202209-240111"
+    dataset = "CUBOID_TEST"
+    version = '4.1.3'
+
+    cuboid_test = "C:/Users/pc/SS-233/hyundai_code/S3_hyundai/3.working_done/CUBOID_TEST/"
+
+
+    print(" 후처리 코드 실행 ")
+    for folder_name in os.listdir(f"{high_path}/{step3_path}/{space}/{sequence_set}"):
+        # file_name = os.listdir(f"{high_path}/{step3_path}/{space}/{sequence_set}/{folder_name}")[0][12:]
+        # print(folder_name)
         # print(file_name)
-        result = file_name.split('-')
+        result = folder_name.split('-')
         company = result[0]
         test_car_num = result[1]
-        log_start_time =  "20" + result[2]
-        print(log_start_time)
+        log_start_time = "20" + result[2]
+        # print(log_start_time)
 
-        property_input_folder = f"{path}/S3_hyundai/2.working_done/{folder_name}/"
-        property_output_folder = f"{path}/S3_hyundai/3.out/{folder_name}/LDR_GT_Property/"
-        property_filename = f"LDR_GT_Property-{file_name}-{version}.json"
+        property_input_folder = f"{high_path}/{step3_path}/{space}/{sequence_set}/{folder_name}/"
+        property_output_folder = f"{high_path}/{step4_path}/{space}/{sequence_set}/{folder_name}/LDR_GT_Property/"
+        # print(property_output_folder)
 
-        box_input_folder = f"{path}/S3_hyundai/2.working_done/{folder_name}/annotations"
-        box_output_folder = f"{path}/S3_hyundai/3.out/{folder_name}/LDR_GT_Box"
-        box_filename = f"LDR_GT_Box-{file_name}-{version}.json"
+        property_filename = f"LDR_GT_Property-{folder_name}-{version}.json"
+
+        box_input_folder = f"{high_path}/{step3_path}/{space}/{sequence_set}/{folder_name}/annotations"
+        box_output_folder = f"{high_path}/{step4_path}/{space}/{sequence_set}/{folder_name}/LDR_GT_Box"
+        box_filename = f"LDR_GT_Box-{folder_name}-{version}.json"
 
         # TODO Property
         # 생성된 폴더 확인 및 생성
-        if not os.path.exists(property_output_folder):
+        if not os.path.exists(f"{cuboid_test}/LDR_GT_Property"):
 
             # 후처리 할 데이터 불러오기
-            data = pd.read_excel(property_input_folder + 'property_data.xlsx')
+            data = pd.read_excel(f"{cuboid_test}/property_data.xlsx")
             data.drop([0, 1], axis=0, inplace=True)
             data.reset_index(drop=True,inplace=True)
-
+            # print(data.loc[0])
             # csv 파일에서 데이터
-            for i in range(len(data)):
-                row = data.loc[i]
-                seq_data = GTP.sequence_metadata(row)
-                sence_data = GTP.sence_curation(row)
-                output_json = GTP.convert_to_new_format(seq_data,sence_data)
+            row = data.loc[0]
+            seq_data = GTP.sequence_metadata(row, version)
+            sence_data = GTP.sence_curation(row)
+            output_json = GTP.convert_to_new_format(seq_data, sence_data)
             # else:
             #     print("error: data[NAME] not found in 'property_data.xlsx'")
             # 파일명과 동일한 이름으로 변환된 JSON 파일 저장
-            os.makedirs(property_output_folder)
-            output_file = os.path.join(property_output_folder, property_filename)
+            os.makedirs(f"{cuboid_test}/LDR_GT_Property")
+            output_file = os.path.join(f"{cuboid_test}/LDR_GT_Property", property_filename)
             with open(output_file, 'w') as f:
                 json.dump(output_json, f, indent=2)
-                # print(f'Save {json_file}')
-
+            print(f" '{folder_name}--LDR_GT_Property' 폴더가 생성 되었습니다. ")
+        else:
+            print(f" '{folder_name}--LDR_GT_Property' 폴더는 이미 생성 되었습니다. ")
         # TODO BOX
         # 생성된 폴더 확인 및 생성
-        if not os.path.exists(box_output_folder):
+        if not os.path.exists(f"{cuboid_test}/LDR_GT_BOX"):
             # 입력 폴더 내의 모든 JSON 파일 목록 가져오기
-            json_files = [f for f in os.listdir(box_input_folder) if f.endswith('.json')]
+            json_files = [f for f in os.listdir(f"{cuboid_test}/annotations") if f.endswith('.json')]
 
             output_json = {"FRAME_LIST": []}
             for idx,json_file in enumerate(json_files):
@@ -62,21 +76,21 @@ if __name__ == '__main__':
                 pcdfilenum = int(json_file[:6])
 
                 # 각 JSON 파일을 읽어오기
-                with open(os.path.join(box_input_folder, json_file), 'r') as f:
+                with open(os.path.join(f"{cuboid_test}/annotations", json_file), 'r') as f:
                     data = json.load(f)
                 # 후처리 함수
-                frame_metadata = GTB.change_frame_metadata(idx, pcdfilenum, log_start_time)
+                frame_metadata = GTB.change_frame_metadata(idx+1, pcdfilenum, log_start_time)
                 object_list = GTB.change_object_list(data)
                 frame = GTB.frames(object_list, frame_metadata)
                 output_json['FRAME_LIST'].append(frame)
 
             # 파일명과 동일한 이름으로 변환된 JSON 파일 저장
-            output_file = os.path.join(box_output_folder, box_filename)
-            os.makedirs(box_output_folder)
+            output_file = os.path.join(f"{cuboid_test}/LDR_GT_BOX", box_filename)
+            os.makedirs(f"{cuboid_test}/LDR_GT_BOX")
             with open(output_file, 'w') as f:
                 json.dump(output_json, f, indent=2)
-            print(f"🆕🆕🆕 '{folder_name}' 폴더 생성 되었습니다. 🆕🆕🆕")
+            print(f" '{folder_name}--LDR_GT_BOX' 폴더가 생성 되었습니다. ")
 
         else:
-            print(f"❗❗❗ '{folder_name}' 폴더는 이미 생성 되었습니다. ❗❗❗")
-    print("💛💛💛 후처리 코드 종료 💛💛💛")
+            print(f" '{folder_name}--LDR_GT_BOX' 폴더는 이미 생성 되었습니다. ")
+    print(" 후처리 코드 종료 ")
