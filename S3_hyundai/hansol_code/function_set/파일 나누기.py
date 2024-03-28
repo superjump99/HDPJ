cd2b135d4f528fb740e7905144949f4feec9a35f
Hansol Kim99 <khs_9902@naver.com>
2024-03-28 10:26:17 +0900

    fix PCDBIN to pcdbin

--- S3_hyundai/hansol_code/function_set/파일 나누기.py
+++ S3_hyundai/hansol_code/function_set/파일 나누기.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
                 # os.makedirs(annotation_folder)
 
                 source_folder = f"{high_path}/{given_path}/{space}/{dataset}/LDR_Raw_PCD/LDR_Raw_PCD-{sequence_set[12:]}/"
-                target_folder = f"{high_path}/{step1_path}/{space}/{dataset}/{sequence_set[12:]}/pointclouds/"
+                target_folder = f"{high_path}/{step1_path}/{space}/{dataset}/{sequence_set[12:]}/pcdbin/"
                 shutil.copytree(source_folder, target_folder)
 
                 source_folder = f"{high_path}/{given_path}/{space}/{dataset}/LDR_Raw_Image/LDR_Raw_Image-{sequence_set[12:]}/ImageFC"
