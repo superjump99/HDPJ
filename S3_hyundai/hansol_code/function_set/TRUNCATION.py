51536ad5140d1c8c0e49412a663f0649985b85e3
Hansol Kim <khs_9902@naver.com>
2024-04-04 17:23:56 +0900

    DONE TRUNCATION.py

--- S3_hyundai/hansol_code/function_set/TRUNCATION.py
+++ S3_hyundai/hansol_code/function_set/TRUNCATION.py
@@ -14,8 +14,11 @@ def load_json_annotations(file_path):
             "category": label['category'],
             "x": label["box3d"]["location"]["x"],
             "y": label["box3d"]["location"]["y"],
+            "z": label["box3d"]["location"]["z"],
+            "OCCLUSION": label['occlusion'],
             "width": label["box3d"]["dimension"]["width"],
             "length": label["box3d"]["dimension"]["length"],
+            "height": label["box3d"]["dimension"]["height"],
             "rotationYaw": label["box3d"]["orientation"]["rotationYaw"]
         }
         for label in data["labels"]
@@ -45,10 +48,11 @@ def calculate_box_vertices(center_x, center_y, width, length, rotationYaw):
 
     return corners_rotated
 
-def plot_field_and_boxes(field, df):
+def truncation(field, df):
     plt.figure(figsize=(10, 8))
     plt.plot(field[:, 0], field[:, 1], '-o', label='Field Boundary')
     field_polygon = Polygon(field)
+    truncation_list = []
     for idx, row in df.iterrows():
         box_vertices = np.array(calculate_box_vertices(row['x'], row['y'], row['length'], row['width'], row['rotationYaw']))
         box_polygon = Polygon(box_vertices)
@@ -60,81 +64,30 @@ def plot_field_and_boxes(field, df):
             rating = difference_area / box_polygon.area
         else:
             rating = 0
-        print(f'Box {idx}', rating)
-    plt.title('Field with Box')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.legend()
-    plt.grid(True)
-    plt.axis('equal')
-    plt.show()
+        truncation_list.append(rating)
 
-def process_annotations(annotations_path, field_data):
-    field = np.array(field_data)
-    json_files = [f for f in os.listdir(annotations_path) if f.endswith('.json')]
-    for json_file in json_files:
-        df = load_json_annotations(os.path.join(annotations_path, json_file))
-        plot_field_and_boxes(field, df)
-        print('----------------------------------------')
+    df['truncation'] = truncation_list
+    return df
+
+    # plt.title('Field with Box')
+    # plt.xlabel('X')
+    # plt.ylabel('Y')
+    # plt.legend()
+    # plt.grid(True)
+    # plt.axis('equal')
+    # plt.show()
+
+
+
 
 if __name__ == '__main__':
     field_data = [(-1, 0), (20, 50), (120, 50), (120, -50), (20, -50), (-1, 0)]
+    field = np.array(field_data)
     annotations_path = "C:/Users/pc/hyundai/input/HYUNDAI/CUBOID_TEST/annotations"
-    process_annotations(annotations_path, field_data)
 
-#     field = np.array(field_data)
-#     field_polygon = Polygon(field_data)
-# print(field_polygon)
-#
-#
-# json_files = [f for f in os.listdir(f"{cuboid_test}/annotations") if f.endswith('.json')]
-#
-# for idx, json_file in enumerate(json_files):
-#     pcdfilenum = int(json_file[:6])
-#
-#     # 각 JSON 파일을 읽어오기
-#     with open(os.path.join(f"{cuboid_test}/annotations", json_file), 'r') as f:
-#         data = json.load(f)
-#     # print(len(data['labels']))
-#     converted_data = [
-#         {
-#             "category": label['category'],
-#             "x": label["box3d"]["location"]["x"],
-#             "y": label["box3d"]["location"]["y"],
-#             "width": label["box3d"]["dimension"]["width"],
-#             "length": label["box3d"]["dimension"]["length"],
-#             "rotationYaw": label["box3d"]["orientation"]["rotationYaw"]
-#         }
-#         for i, label in enumerate(data["labels"])
-#     ]
-#     df = pd.DataFrame(converted_data)
-#     # print(df)
-#     plt.figure(figsize=(10, 8))
-#     plt.plot(field[:, 0], field[:, 1], '-o', label='Field Boundary')
-#     for idx in range(len(df)):
-#         print(df.iloc[idx].tolist()[1:])
-#         boxpoint = df.iloc[idx].tolist()[1:]
-#
-#         box_vertices = np.array(calculate_box_vertices(boxpoint[0], boxpoint[1], boxpoint[2], boxpoint[3], boxpoint[4]))
-#         box_polygon = Polygon(calculate_box_vertices(boxpoint[0], boxpoint[1], boxpoint[2], boxpoint[3], boxpoint[4]))
-#
-#         # print(box_polygon)
-#         plt.plot(box_vertices[:, 0], box_vertices[:, 1], '-s', label=f'Box {idx}')
-#         # plt.fill(box_vertices[:, 0], box_vertices[:, 1], 'b', alpha=0.1, label=f'Box {idx} ')
-#         if not field_polygon.contains(box_polygon):
-#             # print(field_polygon.contains(box_polygon))
-#             # 박스와 필드의 차집합 영역 계산
-#             difference_area = box_polygon.difference(field_polygon).area
-#             rating = difference_area / box_polygon.area
-#             # print(rating,field_polygon.area, difference_area, box_polygon.area)
-#         else:
-#             difference_area = 0
-#
-#
-#     plt.title('Field with Box')
-#     plt.xlabel('X')
-#     plt.ylabel('Y')
-#     plt.legend()
-#     plt.grid(True)
-#     plt.axis('equal')
-#     plt.show()
+    json_files = [f for f in os.listdir(annotations_path) if f.endswith('.json')]
+    for json_file in json_files:
+        df = load_json_annotations(os.path.join(annotations_path, json_file))
+        truncation_df = truncation(field, df)
+        print(truncation_df)
+
