9de5246ff00f236e157f5826e8c341d4eec09010
Hansol Kim <khs_9902@naver.com>
2024-05-29 13:45:27 +0900

    clean push

--- Post_main.py
+++ Post_main.py
@@ -30,12 +30,12 @@ if __name__ == '__main__':
         result = sequence_set.split('-')
         log_start_time = "20" + result[2]
 
-        property_json = f"{date.today()}-LDR_GT_Property-{sequence_set}-{version}.json"
-        box_json = f"{date.today()}-LDR_GT_Box-{sequence_set}-{version}.json"
+        property_json = f"LDR_GT_Property-{sequence_set}-{version}.json"
+        box_json = f"LDR_GT_Box-{sequence_set}-{version}.json"
 
         # TODO Property
         # 생성된 폴더 확인 및 생성
-        if not os.path.exists(f"{base_path}/LDR_GT_Property/{sequence_set}/{property_json}"):
+        if not os.path.exists(f"{base_path}/LDR_GT_Property/{property_json}"):
             # STEP 1. ROAD PROPERTY DATA
             data = pd.read_excel(f"{DATA_path}/{sequence_set}/{sequence_set}.xlsx")
 
@@ -50,17 +50,22 @@ if __name__ == '__main__':
             output_json = PROPERTY.convert_to_new_format(seq_data, scene_data)
 
             # STEP 4. Save json file
-            output_file_path = os.path.join(f"{base_path}/LDR_GT_Property/{sequence_set}", property_json)
-            with open(output_file_path, 'w') as f:
-                json.dump(output_json, f, indent=2)
+            output_file_path = os.path.join(f"{base_path}/LDR_GT_Property/")
+            if not os.path.exists(output_file_path): os.makedirs(output_file_path)
 
-            print(f" [{sequence_set}--LDR_GT_Property] 폴더가 생성 되었습니다. ")
+            save_path = os.path.join(output_file_path, property_json)
+            print(type(save_path))
+            # print(os.path.join(output_file_path, property_json))
+            with open(save_path, 'w') as f:
+                json.dump(output_json, f, indent=4)
+
+            print(f" [LDR_GT_Property/{sequence_set}] 폴더가 생성 되었습니다. ")
         else:
-            print(f" [{sequence_set}--LDR_GT_Property] 폴더는 이미 생성 되었습니다. ")
+            print(f" [LDR_GT_Property/{sequence_set}] 폴더는 이미 생성 되었습니다. ")
 
         # TODO BOX
         # 생성된 폴더 확인 및 생성
-        if not os.path.exists(f"{base_path}/LDR_GT_BOX/{sequence_set}/{box_json}"):
+        if not os.path.exists(f"{base_path}/LDR_GT_Point/{box_json}"):
             field = np.array([(-1, 0), (20, 50), (120, 50), (120, -50), (20, -50), (-1, 0)])
 
             # 입력 폴더 내의 모든 JSON 파일 목록 가져오기
@@ -84,13 +89,15 @@ if __name__ == '__main__':
                     output_json['FRAME_LIST'].append(frame)
 
             # 파일명과 동일한 이름으로 변환된 JSON 파일 저장
-            output_file_path = os.path.join(f"{base_path}/LDR_GT_BOX/{sequence_set}", box_json)
-            # os.makedirs(f"{base_path}/{working_done_path}/{space}/{dataset}/{folder_name}/LDR_GT_BOX")
-            with open(output_file_path, 'w') as f:
+            output_file_path = os.path.join(f"{base_path}/LDR_GT_Point/")
+            if not os.path.exists(output_file_path): os.path.exists(output_file_path)
+            save_path = os.path.join(output_file_path, box_json)
+
+            with open(save_path, 'w') as f:
                 json.dump(output_json, f, indent=2)
 
-            print(f" [{sequence_set}-LDR_GT_BOX] 폴더가 생성 되었습니다.")
+            print(f" [LDR_GT_Point/{sequence_set}] 폴더가 생성 되었습니다.")
 
         else:
-            print(f" [{sequence_set}-LDR_GT_BOX] 폴더는 이미 생성 되었습니다. ")
+            print(f" [LDR_GT_Point/{sequence_set}] 폴더는 이미 생성 되었습니다. ")
     print(" 후처리 코드 종료 ")
