572a3bacd1f12a0f5fabf9d0153ea281968cd29a
Hansol Kim <khs_9902@naver.com>
2024-06-28 17:23:57 +0900

    update: 함수 이름 추가

--- Pre_main.py
+++ Pre_main.py
@@ -2,8 +2,10 @@ import os
 import shutil
 from tqdm import tqdm
 
-from function_set.Remove_unnecessary_file import remove_files, rename_files
+from function_set.remove_unnecessary_file import remove_files, rename_files
 from function_set.Data_parsing_2 import pcdbin_parser, pcdbin_to_pcd
+
+from function_set.copy_raw_data import * # step 2 copy raw pcd and images
 
 if __name__ == '__main__':
     drive = 'C:/Users/pc/Desktop'
