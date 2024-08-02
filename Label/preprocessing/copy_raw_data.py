3c2b9dd83478ee3ae58d1c32bb0ccad80139847e
Hansol Kim99 <khs_9902@naver.com>
2024-08-02 10:32:31 +0900

    raw 이미지 폴더 이름 변경

--- Label/preprocessing/copy_raw_data.py
+++ Label/preprocessing/copy_raw_data.py
@@ -1,6 +1,5 @@
 import shutil
-
-
+from Label.preprocessing.remove_unnecessary_file import *
 def raw_pcd_processing(source_path, folder_name, target_path):
     try:
         extract_files(source_path, target_path)
@@ -15,15 +14,15 @@ def raw_image_processing(source_path, folder_name, target_path):
         for folder in os.listdir(source_path):
             if folder == 'ImageFC':
                 CF_folder = os.path.join(target_path, f'{folder_name}/images/CAM_FRONT')
-                extract_files(f'{source_path}/ImageFC_deid', CF_folder)
+                extract_files(f'{source_path}/ImageFC', CF_folder)
                 renumbering_files(CF_folder, '.jpg')
             elif folder == 'ImageFL':
                 CFL_folder = os.path.join(target_path, f'{folder_name}/images/CAM_FRONT_LEFT')
-                extract_files(f'{source_path}/ImageFL_deid', CFL_folder)
+                extract_files(f'{source_path}/ImageFL', CFL_folder)
                 renumbering_files(CFL_folder, '.jpg')
             elif folder == 'ImageFR':
                 CFR_folder = os.path.join(target_path, f'{folder_name}/images/CAM_FRONT_RIGHT')
-                extract_files(f'{source_path}/ImageFR_deid', CFR_folder)
+                extract_files(f'{source_path}/ImageFR', CFR_folder)
                 renumbering_files(CFR_folder, '.jpg')
             else:
                 pass
