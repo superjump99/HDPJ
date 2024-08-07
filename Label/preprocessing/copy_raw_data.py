a1bbb0e949f793ccaafe01924b605c634473701a
Hansol Kim <khs_9902@naver.com>
2024-08-07 11:53:19 +0900

    필요한 파일만 파싱할 수 있도록 target_file 인자 추가

--- Label/preprocessing/copy_raw_data.py
+++ Label/preprocessing/copy_raw_data.py
@@ -1,28 +1,28 @@
 import shutil
 from Label.preprocessing.remove_unnecessary_file import *
-def raw_pcd_processing(source_path, folder_name, target_path):
+def raw_pcd_processing(source_path, folder_name, target_path, target_file):
     try:
-        extract_files(source_path, target_path)
+        extract_files(source_path, target_path, target_file)
         print(f"[PCD copy completed]")
 
     except FileExistsError:
         print(f"[PCD copy already completed]")
     return
 
-def raw_image_processing(source_path, folder_name, target_path):
+def raw_image_processing(source_path, folder_name, target_path, target_file):
     try:
         for folder in os.listdir(source_path):
             if folder == 'ImageFC':
                 CF_folder = os.path.join(target_path, f'{folder_name}/images/CAM_FRONT')
-                extract_files(f'{source_path}/ImageFC', CF_folder)
+                extract_files(f'{source_path}/ImageFC', CF_folder, target_file)
                 renumbering_files(CF_folder, '.jpg')
             elif folder == 'ImageFL':
                 CFL_folder = os.path.join(target_path, f'{folder_name}/images/CAM_FRONT_LEFT')
-                extract_files(f'{source_path}/ImageFL', CFL_folder)
+                extract_files(f'{source_path}/ImageFL', CFL_folder, target_file)
                 renumbering_files(CFL_folder, '.jpg')
             elif folder == 'ImageFR':
                 CFR_folder = os.path.join(target_path, f'{folder_name}/images/CAM_FRONT_RIGHT')
-                extract_files(f'{source_path}/ImageFR', CFR_folder)
+                extract_files(f'{source_path}/ImageFR', CFR_folder, target_file)
                 renumbering_files(CFR_folder, '.jpg')
             else:
                 pass
@@ -33,13 +33,13 @@ def raw_image_processing(source_path, folder_name, target_path):
     return
 
 
-def extract_files(source_folder, target_folder):
+def extract_files(source_folder, target_folder, target_file):
     os.makedirs(target_folder, exist_ok=True)
 
     for filename in os.listdir(source_folder):
         file_number = ''.join(filter(str.isdigit, filename))
 
-        if file_number.isdigit() and int(file_number) % 25 == 0:
+        if file_number.isdigit() and int(file_number) in target_file:
             file_path = os.path.join(source_folder, filename)
 
             if os.path.isfile(file_path):
